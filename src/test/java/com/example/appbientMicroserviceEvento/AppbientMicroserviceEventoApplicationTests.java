38a2f2972c10bdde5c9b7afe7298d57a3d1b369b
Antonio Ferrándiz <antoniojfb01@gmail.com>
2022-05-31 13:08:20 -0500

    first commit

---
+++ src/test/java/com/example/appbientMicroserviceEvento/AppbientMicroserviceEventoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.appbientMicroserviceEvento;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppbientMicroserviceEventoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
